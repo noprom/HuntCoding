0f766b6f682f68cb751509ca03583c9d40609b54
noprom <874702417@qq.com>
2015-04-22 05:13:08 +0800

    first commit
    first commit

---
+++ src/main/java/com/huntdreams/coding/MainActivity.java
@@ -0,0 +1,17 @@
+package com.huntdreams.coding;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
