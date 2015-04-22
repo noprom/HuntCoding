6dd2b2b98977e98fc6261c334db428ada5d0dd1b
noprom <874702417@qq.com>
2015-04-22 20:42:22 +0800

    初始化entrance activity

---
+++ src/main/java/com/huntdreams/coding/EntranceActivity.java
@@ -0,0 +1,17 @@
+package com.huntdreams.coding;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.entrance_image)
+public class EntranceActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
