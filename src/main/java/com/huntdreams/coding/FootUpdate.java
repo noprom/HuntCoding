e7bea45903be32ff2e731113f77a8cb941a1c16f
noprom <874702417@qq.com>
2015-05-22 05:29:30 +0800

    FootUpdate的接口完成
    FootUpdate的接口完成

---
+++ src/main/java/com/huntdreams/coding/FootUpdate.java
@@ -0,0 +1,15 @@
+package com.huntdreams.coding;
+
+/**
+ * 底部布局更新类
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ *          Created by noprom on 2015/5/22.
+ */
+
+public class FootUpdate {
+    public static interface LoadMore{
+        public void loadMore();
+    }
+}
---
+++ src/main/java/com/huntdreams/coding/common/network/BaseFragment.java
@@ -0,0 +1,50 @@
+package com.huntdreams.coding.common.network;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.huntdreams.coding.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Fragment基类
+ * 封装了图片下载和网络请求
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ *          Created by noprom on 2015/5/22.
+ */
+public class BaseFragment extends Fragment implements NetworkCallback {
+
+
+    public BaseFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+
+    @Override
+    public void parseJson(int code, JSONObject respanse, String tag, int pos, Object data) throws JSONException {
+
+    }
+
+    @Override
+    public void getNetwork(String url, String tag) {
+
+    }
+}
