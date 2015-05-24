889996fab4227a83121747a8d96e58d4ba693669
noprom <874702417@qq.com>
2015-05-24 21:33:24 +0800

    实现NavigationDrawerFragment.NavigationDrawerCallbacks
    实现NavigationDrawerFragment.NavigationDrawerCallbacks

--- src/main/java/com/huntdreams/coding/MainActivity.java
+++ src/main/java/com/huntdreams/coding/MainActivity.java
@@ -1,10 +1,20 @@
 package com.huntdreams.coding;
 
-import android.support.v7.app.ActionBarActivity;
-
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity implements
+        NavigationDrawerFragment.NavigationDrawerCallbacks{
 
+    public static final String TAG = "MainActivity";
+    NavigationDrawerFragment_ mNavigationDrawerFragment;
+    String mTitle;
+
+
+
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+
+    }
 }
